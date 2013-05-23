93daa6def3d8e6e529c731e90f7121af10b7cfa3
Keith Hackbarth <keith.hackbarth@gmail.com>
2013-05-23 15:07:56 -0700

    Second draft of revisions

--- js/datepair.js
+++ js/datepair.js
@@ -1,128 +1,265 @@
-/* jQuery:true */
+/*jshint jquery:true */
 
-$(function () {
-    var checkin,
-        checkout,
-        now,
-        quarterHours,
-        inOneHour;
+/** Utility for correlated start / end date time selectors.
+ *
+ * Currently assumes fields coming from two external picker utilities:
+ * - http://www.eyecon.ro/bootstrap-datepicker
+ * - http://jonthornton.github.io/jquery-timepicker/
+ */
 
-    // Calculate 'now' to the nearest quarter hour
-    now = new Date();
-    quarterHours = Math.round(now.getMinutes() / 15);
-    if (quarterHours === 4)
-    {
-        now.setHours(now.getHours() + 1);
-    }
-    var rounded = (quarterHours * 15) % 60;
-    now.setMinutes(rounded);
+var DatePair = function (shouldSetToNow) {
 
-    // Calcualte one hour from 'now'
-    inOneHour = new Date(now.getTime() + 60 * 60 * 1000); /* ms */
+    /** DOM state used by DatePair.
+     *
+     * DatePair sits upon a boot strap date picker and a JQuery time
+     * picker.  Start and end versions of these pickers provide the UI
+     * for the user and contain all data.
+     *
+     * TODO: Perhaps refine this to pass the four elements into the
+     * constructor function rather than depending on $(...).last().  Not
+     * sure if that buys us anything, but I do feel more secure
+     * explicitly specifying these.
+     */
+    this.startDate = $('.dt_date_start').last();
+    this.startTime = $('.dt_time_start').last();
+    this.endDate = $('.dt_date_end').last();
+    this.endTime = $('.dt_time_end').last();
 
-    // Initialize Date Picker -- Check In
-    checkin = $('.dates.start').datepicker({
-        'autoclose': true,
-        'todayBtn': true,
-        'todayHighlight': true
-    }).on('changeDate', function (ev) {
+    this.priorTimeDelta = NaN;
 
-        // If Start Date > End Date, then update end date
-        var nowUTC = new Date(
-            ev.date.getUTCFullYear(),
-            ev.date.getUTCMonth(),
-            ev.date.getUTCDate(),  0, 0, 0);
-        if (ev.date > checkout.date) {
-            checkout.setDate(nowUTC);
-            checkout.setStartDate(this.value);
+    /** Hook logic into DOM and optionally set initial date time data.
+     */
+    this.initialize = function (shouldSetToNow) {
+
+        // Fix bug with the way Bootstrap overlays carets
+        $('.caret, .arrow_down').on('click', function() {
+            $(this).siblings().trigger('click');
+        });
+
+        // Associate DatePair with elements on page.
+        this.hookUpWidgets();
+
+        // Optionally now.
+        if (shouldSetToNow) {
+            this.setToNow();
         }
+        // Finally, kick the update logic once.
+        this.verifyStartEnd();
+    };
 
-        updateDateTime($(this));        // Update text box
-        $('.time.start')[0].focus();    // Highlight time stamp
-    }).data('datepicker');
-    checkin.setDate(now);
-    checkin.setStartDate(now);
+    /** Set entry time to now with a duration of one hour.
+     */
+    this.setToNow = function () {
+        var now,
+            inOneHour,
+            quarterHours,
+            rounded;
+        // Calculate 'now' to the nearest quarter hour
+        now = new Date();
+        quarterHours = Math.round(now.getMinutes() / 15);
+        if (quarterHours === 4) {
+            now.setHours(now.getHours() + 1);
+        }
+        rounded = (quarterHours * 15) % 60;
+        now.setMinutes(rounded);
+        // Calculate one hour from 'now'
+        inOneHour = new Date(now.getTime() +  this.oneHourMS);
+        // Update widget state
+        this.setStartDateTime(now);
+        this.setEndDateTime(inOneHour);
+    };
 
-    // Initialize Date Picker -- Check Out
-    checkout = $('.dates.end').datepicker({
-        'autoclose': true
-    }).on('changeDate', function () {
-        updateDateTime($(this));      // Update text box
-        $('.time.end')[0].focus();    // Highlight time stamp
-    }).data('datepicker');
-    checkout.setStartDate('+0d');
-    checkout.setDate(inOneHour);
+    /** Utility configuring widgets associated with DOM elements.
+     */
+    this.hookUpWidgets = function () {
+        var thisDatePair = this;
 
-    // Initialiaze Time Picker -- Check In
-    $('.time.start').timepicker({
-        'timeFormat': 'g:ia',
-        'scrollDefaultNow': true
-    }).on('changeTime', function () {
-        updateDateTime($(this));
-    }).timepicker('setTime', now);
+        // Starti date and time
+        this.startDate.datepicker({
+            'autoclose': true,
+            'todayBtn': true,
+            'todayHighlight': true,
+            'startDate': '+0d',
+            'endDate': '+2m'
+        }).change(function () {
+            thisDatePair.updateEndTime(thisDatePair.priorTimeDelta);
+            thisDatePair.verifyStartEnd();    // Update text box
+            thisDatePair.startTime[0].focus();    // Highlight time stamp
+        });
+        this.startTime.timepicker({
+            'timeFormat': 'g:ia',
+            'scrollDefaultNow': true
+        }).change(function () {
+            thisDatePair.updateEndTime(thisDatePair.priorTimeDelta);
+            thisDatePair.verifyStartEnd();    // Update text box
+        });
 
-    // Initialiaze Time Picker -- Check In
-    $('.time.end').timepicker({
-        'timeFormat': 'g:ia',
-        'scrollDefaultNow': true
-    }).on('changeTime', function () {
-        updateDateTime($(this));
-    }).timepicker('setTime', inOneHour);
+        // End date and time
+        this.endDate.datepicker({
+            'autoclose': true,
+            'startDate': '+0d',
+            'endDate': '+2m'
+        }).change(function () {
+            thisDatePair.verifyStartEnd();   // Update text box
+            thisDatePair.priorTimeDelta = thisDatePair.getTimeDelta();
+            thisDatePair.endTime[0].focus();    // Highlight time stamp
+        }).data('datepicker');
+        this.endTime.timepicker({
+            'timeFormat': 'g:ia',
+            'scrollDefaultNow': true
+        }).change(function () {
+            thisDatePair.priorTimeDelta = thisDatePair.getTimeDelta();
+            thisDatePair.verifyStartEnd();    // Update text box
+        });
+    };
 
+    /** Adjust end time to maintain constant timeDelta from start time.
+     *
+     * Expected behavior of the DatePair widget is that a change to the
+     * start time will result in a modified end time such that the
+     * timeDelta between start and end remains constant.
+     *
+     * @param {number} Time delta in seconds that should exist between
+     * start and end date time.
+     */
+    this.updateEndTime = function (timeDelta) {
+        var startSeconds,
+            endDateTime;
+        if (isNaN(timeDelta) || timeDelta <= 0) {
+            timeDelta = this.oneHourMS / 1000;
+        }
+        endDateTime = new Date(
+                this.getStartDateTime().getTime() +
+                timeDelta * 1000);
+        this.setEndDateTime(endDateTime);
+        // If time delta is less than one day then show duration in
+        // end time picker, else use a minimal display.
+        if (timeDelta < (this.oneHourMS * 24 / 1000)) {
+            startSeconds = this.startTime.timepicker('getSecondsFromMidnight');
+            // TODO: Add duration view back in once github issue 1408 is
+            // resolved.
+            this.endTime.timepicker('option', 'showDuration', false);
+            //this.endTime.timepicker('option', 'minTime', startSeconds + 30 * 60);
+            //this.endTime.timepicker('option', 'durationTime', startSeconds);
+            //this.endTime.timepicker('option', 'showDuration', true);
+        } else {
+            this.endTime.timepicker('option', 'minTime', null);
+            this.endTime.timepicker('option', 'showDuration', false);
+        }
+    };
 
-    // Finally, kick the update logic once
-    updateDateTime($('.dates.end'));
+    /** Calculate time delta between start and end date times.
+     *
+     * @returns {number|NaN} Time delta in seconds.  Return NaN if one
+     * of start or end date is not well defined.
+     */
+    this.getTimeDelta = function () {
+        var datetimeDelta;
+        // Determine the datetime delta
+        datetimeDelta = (this.getEndDateTime() - this.getStartDateTime()) / 1000;
+        return datetimeDelta;
+    };
 
-});
+    /** Accessor to get the start Date object.
+     *
+     * returns {Date} Date and time of start.
+     */
+    this.getStartDateTime = function () {
+        var date,
+            startDate,
+            startSeconds;
+        startDate = new Date(this.startDate.val());
+        startSeconds = this.startTime.timepicker('getSecondsFromMidnight');
+        date = new Date(startDate.getTime() + startSeconds * 1000);
+        return date;
+    };
 
-var updateDateTime = function (target) {
-    var container,
-      start,
-      end,
-      startInt,
-      endInt,
-      startTime,
-      endTime,
-      datetimeDelta,
-      dateString,
-      timeString;
+    /** Accessor to get the end Date object.
+     *
+     * returns {Date} Date and time of end.
+     */
+    this.getEndDateTime = function () {
+        var date,
+            endDate,
+            endSeconds;
+        endDate = new Date(this.endDate.val());
+        endSeconds = this.endTime.timepicker('getSecondsFromMidnight');
+        date = new Date(endDate.getTime()  + endSeconds * 1000);
+        return date;
+    };
 
-    // Empty chcek
-    if (!target.val()) {
-        return;
-    }
+    /** Set the start data from Date object.
+     *
+     * param {Date} Start Date object.
+     */
+    this.setStartDateTime = function (date) {
+        var startDate;
+        startDate = new Date(
+                date.getFullYear(), date.getMonth(), date.getDate());
+        this.startDate.data('datepicker').setDate(startDate);
+        this.startTime.timepicker('setTime', date);
+    };
 
-    // Initialize all the function variables
-    container = target.closest('.datepair');
-    start = container.find('input.start.time');
-    end = container.find('input.end.time');
-    startInt = start.timepicker('getSecondsFromMidnight');
-    endInt = end.timepicker('getSecondsFromMidnight');
+    /** Set the end data from Date object.
+     *
+     * param {Date} End Date object.
+     */
+    this.setEndDateTime = function (date) {
+        var endDate;
+        endDate = new Date(
+                date.getFullYear(), date.getMonth(), date.getDate());
+        this.endDate.data('datepicker').setDate(endDate);
+        this.endTime.timepicker('setTime', date);
+        this.priorTimeDelta = this.getTimeDelta();
+    };
 
-    // Determine the datetime delta
-    startTime = new Date($('.dates.start').val()).getTime() + startInt * 1000;
-    endTime = new Date($('.dates.end').val()).getTime() + endInt * 1000;
-    datetimeDelta = endTime - startTime;
+    /** Force end date time to be after start date time.
+     */
+    this.verifyStartEnd = function () {
+        var timeDelta;
+        timeDelta = this.getTimeDelta();
+        // Nothing to do if timeDelta is NaN.
+        if (isNaN(timeDelta)) {
+            return;
+        }
+        // Start must be before end.  If not then shift end.
+        if (timeDelta <= 0) {
+            timeDelta = this.oneHourMS / 1000;
+            this.updateEndTime(timeDelta);
+        }
+        this.displayDuration(timeDelta * 1000);
+    };
 
-    // Date Delta Logic
-    dateString = (datetimeDelta / 86400000).toFixed(0) + ' days ';
+    this.displayDuration = function (datetimeDelta) {
+        var dateString,
+            hourString,
+            minuteString;
 
-    // Time Delta Logic
-    if (datetimeDelta <= 0) {
-        //
-        end.timepicker('setTime', new Date(start.timepicker('getTime').getTime() + 15 * 60000));
-        updateDateTime($('.dates.end'));
-        return;
-    } else if (datetimeDelta < 86400000) {
-        end.timepicker('option', 'minTime', startInt + 30 * 60); // 30 min from start time
-        end.timepicker('option', 'durationTime', startInt);
-        end.timepicker('option', 'showDuration', true);
-    } else {
-        end.timepicker('option', 'minTime', null);
-        end.timepicker('option', 'showDuration', false);
-    }
+        dateString = Math.floor(datetimeDelta / (this.oneHourMS * 24)) + ' days';
+        dateString = (dateString === '1 days') ? '1 day': dateString;
+        dateString = (dateString === '0 days') ? '': dateString;
 
-    timeString = ((datetimeDelta % 86400000) / 3600000).toFixed(1) + ' hours';
+        hourString = Math.floor((datetimeDelta % (this.oneHourMS * 24)) / this.oneHourMS) + ' hours';
+        hourString = (hourString === '1 hours') ? '1 hour': hourString;
+        hourString = (hourString === '0 hours') ? '': hourString;
 
-    $('.output-duration').text(dateString + timeString);
+        minuteString = Math.floor((datetimeDelta % this.oneHourMS) / (this.oneHourMS / 60)) + ' minutes';
+        minuteString = (minuteString === '1 minutes') ? '1 minute': minuteString;
+        minuteString = (minuteString === '0 minutes') ? '': minuteString;
+
+        $('.output-duration').text(dateString + ' ' + hourString + ' ' + minuteString);
+
+    };
+
+    this.oneHourMS = 60 * 60 * 1000;  // one hour in ms
+
+    // constructor
+    (function (thisDatePair) {
+        thisDatePair.initialize(shouldSetToNow);
+    }(this));
+
 };
+
+$(function() {
+    var datePair = new DatePair(true);
+});
